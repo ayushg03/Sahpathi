3be07b77706f2c339a80ad345cd3fbf1817dd5d4
ayushg03 <80210243+ayushg03@users.noreply.github.com>
2023-01-08 10:51:27 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -40,11 +40,12 @@ app.use('/api',require('./routes/events'));
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, '/Client/build')));
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/Client/build/index.html'))
-);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../Client","build")));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../Client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT,()=>{
     console.log(`Listening on port ${PORT}`);
